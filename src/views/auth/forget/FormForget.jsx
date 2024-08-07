5a47bb1b75528903d1a9f2246fa13eecc2dd827c
aluno <tsm6@discente.ifpe.edu.br>
2024-08-07 16:06:44 -0300

    resolvendo bug de puxar para site

--- src/views/auth/forget/FormForget.jsx
+++ src/views/auth/forget/FormForget.jsx
@@ -21,7 +21,7 @@ const handleEmailChange = (e) => {
 // Definir uma função de validação
 const validateEmail = (email) => {
   const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i;
+    /^(([^<>().,;:\s@"]+(\.[^<>().,;:\s@"]+)*)|(".+"))@(([^<>.,;:\s@"]+\.)+[^<>.,;:\s@"]{2,})$/i;
   return re.test(String(email).toLowerCase());
 };
   return (
--- src/views/auth/usuario/register/FormUsuarioRegister.jsx
+++ src/views/auth/usuario/register/FormUsuarioRegister.jsx
@@ -32,7 +32,7 @@ const FormUsuarioRegister = () => {
   // Definir uma função de validação
   const validateEmail = (email) => {
     const re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i;
+      /^(([^<>().,;:\s@"]+(\.[^<>().,;:\s@"]+)*)|(".+"))@(([^<>.,;:\s@"]+\.)+[^<>.,;:\s@"]{2,})$/i;
     return re.test(String(email).toLowerCase());
   };
 
