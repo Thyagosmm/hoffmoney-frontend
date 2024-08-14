554f4a70c1e72b316125a5e1b52167e98359246f
aluno <tsm6@discente.ifpe.edu.br>
2024-08-14 10:25:43 -0300

    eliminando caracteres pra mandar pro site

--- src/views/auth/usuario/update/FormUsuarioUpdate.jsx
+++ src/views/auth/usuario/update/FormUsuarioUpdate.jsx
@@ -57,7 +57,7 @@ const FormUsuarioUpdate = () => {
   // Definir uma função de validação
   const validateEmail = (email) => {
     const re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i;
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i;
     return re.test(String(email).toLowerCase());
   };
 
