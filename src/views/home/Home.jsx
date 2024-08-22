0238076daac1a0e8c04527bf6defdcd451a68128
validName <>
2024-08-22 14:04:32 -0300

    importar css home

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import AppMenu from "../components/appMenu/AppMenu.jsx";
 import Dashboard from "../dashboard/Dashboard.jsx";
 import Landing from "../landing/Landing.jsx";
-
+import "./Home.css";
 const Home = () => {
   const [isLogged, setIsLogged] = useState(false);
   useEffect(() => {
