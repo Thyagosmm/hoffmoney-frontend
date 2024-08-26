e0e5e9a272d59e6643a7c262ba19d8fc4a4b9973
aluno <tsm6@discente.ifpe.edu.br>
2024-08-26 16:44:07 -0300

    arrumando bugs de ronaldo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-import "url-search-params-polyfill";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import "./index.css";
-
-// Polyfills para fs e crypto
-import { Buffer } from 'buffer';
-import process from 'process';
-
-window.Buffer = Buffer;
-window.process = process;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
